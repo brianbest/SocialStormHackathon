b28c41fbf81bd8dc9c91ce4c2b7aa4cc0c3285c7
Brian best <brian.brianb64@gmail.com>
2014-11-21 23:47:01 -0400

    Socket now pushes id to client

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var redis = require("redis");
-//var client = redis.createClient();
+var redis = require("redis");
+var client = redis.createClient();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/app/index.html');
 });
-
+app.use("/roomFull", express.static(__dirname + '/app/roomFull.html'));
 app.use("/styles", express.static(__dirname + '/app/styles'));
 app.use("/scripts", express.static(__dirname + '/app/scripts'));
 app.use("/elements", express.static(__dirname + '/app/elements'));
@@ -22,11 +22,18 @@ io.on('connection', function(socket){
   console.log('a user connected');
 
   //if the room is full then redirect user out
-  var clientr = io.sockets.clients();
-  if (clientr.length <= 1){
-    io.emit('redirect', 'stuff');
+  //var clientr
+    var clientr = io.sockets.server.eio.clientsCount;
+    console.log('end');
+  if (clientr === 3){
+    console.log('OVER!!!');
+    //io.sockets.socket(socketId).
+    //io.emit('redirect', 'stuff');
   }
 
+  //Send client their id
+  var clientID = socket.id;
+  io.to(clientID).emit("logon", clientID);
 
 
   socket.on("chat message", function(msg){
